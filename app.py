8fb13c2a72fef5b55bfa6eb4d429c96d1240b15c
Your Name <your_email@example.com>
2025-07-03 23:43:49 -0400

    init

---
+++ app.py
@@ -0,0 +1,10 @@
+import mcode
+
+
+needs_conversion = input("Enter a string to convert to Morse code:")
+
+for char in needs_conversion.upper():
+    if char in mcode.MORSE_CODE_DICT:
+        print(mcode.MORSE_CODE_DICT[char])
+    else:
+        print(f"Character '{char}' not found in Morse code dictionary.")
